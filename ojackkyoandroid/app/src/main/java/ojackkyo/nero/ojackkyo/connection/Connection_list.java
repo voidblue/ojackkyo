7822ae9542c5c29cc288e9d6005bda6d60ba74d9
DESKTOP-R35GG5T\wjdal <wjdals30531@gmail.com>
2018-08-08 16:40:13 +0900

    로그인 테스트 완료, 게시글 작성 테스트 완료, 게시글 태그 삽입 테스트 중, 닉네임 얻어오기 완료

--- ojackkyoandroid/app/src/main/java/ojackkyo/nero/ojackkyo/connection/Connection_list.java
+++ ojackkyoandroid/app/src/main/java/ojackkyo/nero/ojackkyo/connection/Connection_list.java
@@ -1,9 +1,12 @@
-package ojackkyo.nero.ojackkyo;
+package ojackkyo.nero.ojackkyo.connection;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -12,49 +15,50 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import ojackkyo.nero.ojackkyo.UserInfo;
+
 import static android.content.ContentValues.TAG;
 
 /**
  * Created by wjdal on 2018-08-02.
  */
 
-public class Connection extends AsyncTask{
-    @Override
-    protected Object doInBackground(Object[] objects) {
-        String serverURL = "http://117.17.102.131:4000/auth/login";
-        try {
+public class Connection_list extends AsyncTask{
 
+    private String path = "";
+    private String method = "";
+
+    @Override
+    protected String doInBackground(Object[] objects) {
+
+        String serverURL = "http://117.17.102.131:4000/"+objects[1];  // 그거 1개
+        try {
             URL url = new URL(serverURL);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
 
             httpURLConnection.setRequestProperty("content-type", "application/json; charset=utf-8");
+            httpURLConnection.setRequestProperty("token", (String) objects[3]);
             httpURLConnection.setReadTimeout(5000);
             httpURLConnection.setConnectTimeout(5000);
-            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestMethod((String) objects[2]); // 방식 2개
             httpURLConnection.connect();
 
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("uid", "test");
-            jsonObject.put("password", "test");
+            JsonObject jsonobject = (JsonObject) objects[0]; // 객체 이름 3개
 
             OutputStream outputStream = httpURLConnection.getOutputStream();
-            outputStream.write(jsonObject.toString().getBytes("UTF-8"));
+            outputStream.write(jsonobject.toString().getBytes("UTF-8"));
             outputStream.flush();
             outputStream.close();
 
-
             int responseStatusCode = httpURLConnection.getResponseCode();
-            Log.d(TAG, "POST response code - " + responseStatusCode);
 
             InputStream inputStream;
             if (responseStatusCode == HttpURLConnection.HTTP_OK) {
                 inputStream = httpURLConnection.getInputStream();
                 Log.d(TAG, "********************HTTP_OK**********************");
-                Log.d("l", inputStream.toString());
             } else {
                 inputStream = httpURLConnection.getErrorStream();
             }
-
 
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -66,17 +70,21 @@ public class Connection extends AsyncTask{
                 sb.append(line);
             }
 
-            Log.d("", sb.toString());
+            Gson gson = new Gson();
+            JsonElement jsonElement = gson.fromJson(sb.toString(),JsonElement.class);
+            JsonArray result = jsonElement.getAsJsonArray();
+
             bufferedReader.close();
 
-            return sb.toString();
-
+            return result.toString();
 
         } catch (Exception e) {
 
-            Log.d(TAG, "InsertData: Error ", e);
+            JsonObject jsonObject1 = new JsonObject();
+            jsonObject1.addProperty("error", e.getMessage());
 
-            return new String("Error: " + e.getMessage());
+            return jsonObject1.toString();
         }
+
     }
 }
